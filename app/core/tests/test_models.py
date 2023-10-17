cf188c02f4306b3858f97b903cdab5df93c0d76a
Guru Manglam Siddhe <gurumanglam035@gmail.com>
2023-10-18 01:08:21 +0530

    admin interface setup

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -28,3 +28,15 @@ class ModelTests(TestCase):
         for email, expected in sample_emails:
             user = get_user_model().objects.create_user(email, 'sample123')
             self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('','test123')
+
+    def test_create_superuser(self):
+        user=get_user_model().objects.create_superuser(
+            'test@gmail.com',
+            'test@123',
+        )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)     
